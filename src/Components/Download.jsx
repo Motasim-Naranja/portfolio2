0c413567aba7fb16a29fe611881298d6d761e575
Motasim Ejaz <motasimejaz@gmail.com>
2024-10-06 14:21:47 +0500

    add download button

--- src/Components/Download.jsx
+++ src/Components/Download.jsx
@@ -5,7 +5,9 @@ import { BsDownload } from 'react-icons/bs'
 const Download = () => {
   return (
     <>
-      <div> <BsDownload/></div>
+      <div className='rounded-[50%] h-10 w-10 flex items-center px-[9px] fixed bottom-[5vh] right-[5vw] text-xl bg-[#7843E9] text-white'>
+         <BsDownload/>
+        </div>
     </>
   )
 }
