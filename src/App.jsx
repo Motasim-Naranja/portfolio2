09aea76b08e03986b8454026cbcac19f2b58a6ef
Motasim Ejaz <motasimejaz@gmail.com>
2024-10-08 14:21:42 +0500

    initialize project section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './Components/Navbar'
 import Home from './Components/Home'
 import About from './Components/About'
 import Download from './Components/Download'
+import Projects from './Components/Projects'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Navbar/>
       <Home/>
       <About/>
+      <Projects/>
 
       <Download/>
     </>
---
+++ src/Components/Projects.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <>
+        <div className='h-[100vh] text-xl'>
+            <h2 className='text-3xl font-bold'>PROJECTS</h2>
+            <p>Here you will find some of the personal and clients projects that I created with each project containing its own case study</p>
+        </div>
+    </>
+  )
+}
+
+export default Projects
