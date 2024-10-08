51ecd3801abe572a74b2573ddcac4a5a69d0be8c
Motasim Ejaz <motasimejaz@gmail.com>
2024-10-08 21:32:13 +0500

    projects updated

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -8,15 +8,26 @@ import universal_stationary from '../assets/universal stationary.jpg'
 const Projects = () => {
     return (
         <>
-            <div className='h-[100vh] text-xl bg-[#f8f8f8] py-12 '>
-                <h2 className='text-3xl font-extrabold text-center'>PROJECTS</h2>
-                <p className='text-center text-gray-600' >Here you will find some of the personal and clients projects that I created with each project containing its own case study</p>
+            <div className='text-xl bg-[#7434ff] py-12 '>
+                <h2 className='text-3xl font-extrabold text-center text-white'>PROJECTS</h2>
+                <p className='text-center  text-white ' >Here you will find some of the personal and clients projects that I created with each project containing its own case study</p>
 
-                <div className='bg-black py-2 flex'>
-                    <div className='w-[90vw] mx-[auto] px-[30px]  flex gap-16 bg-slate-400 py-12'>
-
+                <div className=' py-2 flex flex-col justify-around '>
+                    
+                    <div className='w-[90vw] mx-auto px-[30px] flex gap-16 bg-[#f3f2f2d0] py-12 rounded-[20px] backdrop-blur-lg border border-white shadow-white-lg'>
                         <img src={portfolio} alt="" className='rounded-[20px]  h-[40vh]' />
-                        <div className='tracking-wider'>
+                        <div className='tracking-wider flex flex-col gap-3'>
+                            <h4 className='font-extrabold text-2xl'>Portfolio</h4>
+                            <p className='font-medium'>
+                            I built this portfolio as a dashboard-style web app using React and Tailwind CSS. It’s fully responsive, with sections for my bio, skills, projects, and contact details. The sleek, modern design highlights my frontend development skills and showcases my ability to create dynamic, user-friendly interfaces.
+                            </p>
+                        </div>
+
+                    </div>
+
+                    <div className='w-[90vw] mx-auto px-[30px] flex gap-16 bg-[#f3f2f2d0] py-12 rounded-[20px] backdrop-blur-lg border border-white shadow-white-lg'>
+                        <img src={portfolio} alt="" className='rounded-[20px]  h-[40vh]' />
+                        <div className='tracking-wider flex flex-col gap-3'>
                             <h4 className='font-extrabold text-2xl'>Portfolio</h4>
                             <p className='font-medium'>
                             I built this portfolio as a dashboard-style web app using React and Tailwind CSS. It’s fully responsive, with sections for my bio, skills, projects, and contact details. The sleek, modern design highlights my frontend development skills and showcases my ability to create dynamic, user-friendly interfaces.
